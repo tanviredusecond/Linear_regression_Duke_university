68931d60a3e0d6cbfb356408768e2c57d88fe8b4
mechanic <ornobtanvir.git@gmail.com>
2020-01-26 19:46:42 +0600

    added

---
+++ first.R
@@ -0,0 +1,6 @@
+## if the predicted value of the response variable for a given data point
+## is above the observed value then the model is overestimating
+## your value is low but you expect high then it is overeastimating
+
+
+## slope = ((standerd deviation of x ) / (standerd deviation of Y)) * correlation
